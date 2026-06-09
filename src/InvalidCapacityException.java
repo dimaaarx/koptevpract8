bac2bdf25a26fc0097a4779b694e47bf0b2c27cf
dimaaarx <koptevdima1107@gmail.com>
2026-06-09 22:34:55 +0300

    Update InvalidCapacityException.java

--- src/InvalidCapacityException.java
+++ src/InvalidCapacityException.java
@@ -1,4 +1,5 @@
-package PACKAGE_NAME;
-
-public class InvalidCapacityException {
+class InvalidIndexException extends RuntimeException {
+    public InvalidIndexException(String message) {
+        super(message);
+    }
 }
